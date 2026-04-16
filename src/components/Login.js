fae7ccfd8da1b18866bac051aafe4677a28d2042
Himanshu Singh <himanshuchahar3318@gmail.com>
2026-04-16 21:27:33 +0530

    signIn/signUp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+const[isSignin, setisSignin]=useState(true)
+
+const toggle=()=>{
+    setisSignin(!isSignin)
+}
+
+
   return (
     <div className="relative">
       <img
@@ -13,7 +20,14 @@ const Login = () => {
 
       <div className="absolute inset-0 flex items-center justify-center">
         <form className="w-11/12 max-w-md rounded bg-black/75 p-12 text-white">
-          <h1 className="mb-8 text-5xl font-bold">Sign In</h1>
+        <h1 className="mb-8 text-5xl font-bold">{isSignin ? "Sign In" : "Sign Up"}</h1>
+ 
+       {!isSignin && <input
+            type="text"
+            placeholder="Name"
+            className="mb-4 w-full rounded bg-gray-700/90 p-4 text-lg outline-none placeholder:text-gray-300"
+          />}
+
 
           <input
             type="text"
@@ -26,8 +40,11 @@ const Login = () => {
             className="mb-8 w-full rounded bg-gray-700/90 p-4 text-lg outline-none placeholder:text-gray-300"
           />
 
-          <button className="mb-4 w-full rounded bg-red-600 p-4 text-2xl font-semibold hover:bg-red-700">
-            Sign In
+          <button
+            type="button"
+            className="mb-4 w-full rounded bg-red-600 p-4 text-2xl font-semibold hover:bg-red-700"
+          >
+            {isSignin ? "Sign In" : "Sign Up"}
           </button>
 
           <div className="mb-12 flex items-center justify-between text-xl text-gray-300">
@@ -39,7 +56,13 @@ const Login = () => {
           </div>
 
           <p className="mb-4 text-xl text-gray-400">
-            New to Netflix? <span className="font-semibold text-white">Sign up now.</span>
+            New to Netflix?{" "}
+            <span
+              className="cursor-pointer font-semibold text-white"
+              onClick={toggle}
+            >
+              Sign up now.
+            </span>
           </p>
           <p className="text-lg text-gray-400">
             This page is protected by Google reCAPTCHA to ensure you are not a bot.{" "}
